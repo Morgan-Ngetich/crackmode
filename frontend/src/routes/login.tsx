f9b6007d775e5841238fa8424b585712db391b26
Morgan <ngetichmorgan6@gmail.com>
2026-02-16 18:03:32 +0300

    Refactor to better handle redict logic.

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -3,16 +3,22 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { Suspense } from 'react';
 import LoginForm from "../pages/LoginForm";
 import { isLoggedIn } from "../hooks/auth/authState";
+import { z } from "zod"
+
+const loginSearchSchema = z.object({
+  redirectTo: z.string().optional()
+})
 
 export const Route = createFileRoute("/login")({
+  validateSearch: loginSearchSchema,
   component: () => (
     <Suspense fallback={<Spinner />}>
       <LoginForm />
     </Suspense>
   ),
-  beforeLoad: async () => {
+  beforeLoad: async ({ search }) => {
     if (await isLoggedIn()) {
-      throw redirect({ to: "/" });
+      throw redirect({ to: search.redirectTo || "/" });
     }
   }
 });
--- frontend/src/routes/signup.tsx
+++ frontend/src/routes/signup.tsx
@@ -1,14 +1,25 @@
 import { Spinner } from "@chakra-ui/react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 import { Suspense } from 'react';
 import SignupForm from "../pages/SignupForm";
-// import { isLoggedIn } from "../hooks/auth/authState";
+import { isLoggedIn } from "../hooks/auth/authState";
+import { z } from "zod";
+
+const signupSearchSchema = z.object({
+  redirectTo: z.string().optional(),
+});
 
 export const Route = createFileRoute("/signup")({
+  validateSearch: signupSearchSchema,
   component: () => (
     <Suspense fallback={<Spinner />}>
       <SignupForm />
     </Suspense>
-
   ),
+  beforeLoad: async ({ search }) => {
+    if (await isLoggedIn()) {
+      // Redirect to the intended destination or home
+      throw redirect({ to: search.redirectTo || "/" });
+    }
+  }
 });
